b08043012b75e9c62eaf60d20d46159f574f6ebe
bishnudev <devbishnu2000@gmail.com>
2021-12-09 14:43:12 +0400

    Initial release of v2.0.0
    - Added pop-up for editing item
    - Added "disabled" property support.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import {useState} from "react";
 
 function App() {
-    const [suggestions, setSuggestions] = useState([{name: 'Microsoft'}, {name: 'Apple'}, {name: 'Google'}, {name: 'JetBrains'}, {name: 'Airbnb'}, {name: 'Amazon'}, {name: 'Tesla'}, {name: 'NVIDIA'}, {name: 'Samsung'}, {name: 'Netflix'}, {name: 'Palantir'}]);
+    const [suggestions, setSuggestions] = useState([{name: 'Microsoft', id:1}, {name: 'Apple', id:2}, {name: 'Google', id:3}, {name: 'JetBrains', id:4}, {name: 'Airbnb', id:5}, {name: 'Amazon', id:6}, {name: 'Tesla', id:7}, {name: 'NVIDIA', id:8}, {name: 'Samsung', id:9}, {name: 'Netflix', id:10}, {name: 'Palantir', id:11}]);
     const [selectedList, setSelectedList] = useState([]);
 
     return (
@@ -11,14 +11,24 @@ function App() {
             <MultiSelect
                 suggestions={suggestions}
                 selectedItems={selectedList}
-                updateSuggestions={(newList, added, removed) => {
-                    setSuggestions(newList)
+                updateSuggestions={(response) => {
+                    console.log("suggestion: ")
+                    console.log(response);
+                    setSuggestions(response.list);
                 }}
-                updateSelectedItems={(newList, added, removed, isCreated) => {
-                    setSelectedList(newList)
+                updateSelectedItems={(response) => {
+                    console.log("selection: ")
+                    console.log(response);
+                    // let's add removed items to the suggestion
+                    if (response.removedItem){
+                        setSuggestions([...suggestions, response.removedItem]);
+                    }
+                    setSelectedList(response.list);
                 }}
                 displayField={"name"}
                 maxDisplayedItems={5}
+                disabled={false}
+                editFieldPosBelow={true}
                 placeholder={"Type the name of the desired company or choose one."}/>
         </div>
     );
